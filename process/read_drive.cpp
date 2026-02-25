c2026edaa250469e71be5cfad327fec5560de784
chro11os <neilbragsg@gmail.com>
2026-02-25 15:19:30 +0800

    feat: Integrate FTXUI and introduce a terminal user interface, refactoring `main.cpp` to utilize the new UI and adding `read_drive` and file utility components.

---
+++ process/read_drive.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Neil Brags Guzman on 2/25/26.
+//
+
+#include "read_drive.h"
---
+++ process/read_drive.h
@@ -0,0 +1,8 @@
+//
+// Created by Neil Brags Guzman on 2/25/26.
+//
+#include <iostream>
+#ifndef MODULO_READ_DRIVE_H
+#define MODULO_READ_DRIVE_H
+int read_drive(std::string drive);
+#endif //MODULO_READ_DRIVE_H
